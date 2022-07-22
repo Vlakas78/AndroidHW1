23ec8388510ad13ddf6ff63ba848ecbcd6005a69
Vladislav Kashpersky <vlakas1978@yandex.ru>
2022-07-22 06:35:55 +0300

    Add fragment

---
+++ app/src/main/java/ru/netology/nmedia/UI/AppActivity.kt
@@ -0,0 +1,14 @@
+package ru.netology.nmedia.UI
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import ru.netology.nmedia.FeedFragment
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.AppActivityBinding
+
+class AppActivity : AppCompatActivity(R.layout.app_activity) {
+
+
+
+}
