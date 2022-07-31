e7f38409ec127ea6fa672d26fc4ef1c0dc81d482
Vladislav Kashpersky <vlakas1978@yandex.ru>
2022-07-31 06:30:58 +0300

    Changes postContetntFrag

--- app/src/main/java/ru/netology/nmedia/UI/PostContentFragment.kt
+++ app/src/main/java/ru/netology/nmedia/UI/PostContentFragment.kt
@@ -9,58 +9,69 @@ import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import ru.netology.nmedia.databinding.PostContentFragmentBinding
 import ru.netology.nmedia.util.focusAndShowKeyboard
+import androidx.lifecycle.ViewModel
 
 class PostContentFragment : Fragment() {
 
     private val args by navArgs<PostContentFragmentArgs>()
 
-      override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ) = PostContentFragmentBinding.inflate(layoutInflater, container, false).also { binding ->
-          binding.edit.setText(args.initialContent)
-          binding.edit.focusAndShowKeyboard()
-          binding.ok.setOnClickListener {
-          onOkButtonClicked(binding)
-          }
+        binding.edit.setText(args.initialContent)
+        binding.edit.focusAndShowKeyboard()
+        binding.ok.setOnClickListener {
+            onOkButtonClicked(binding)
+        }
     }.root
 
 
-
-private fun onOkButtonClicked(binding:PostContentFragmentBinding) {
+    private fun onOkButtonClicked(binding: PostContentFragmentBinding) {
         val intent = Intent()
         val text = binding.edit.text
         if (!text.isNullOrBlank()) {
             val resultBundle = Bundle(1)
             resultBundle.putString(RESULT_KEY, text.toString())
 
-            if (args.fromFragment == REQUEST_KEY) {
-                setFragmentResult(
-                    REQUEST_KEY,
-                    resultBundle
+            setFragmentResultListener(
+                requestKey = PostContentFragment.REQUEST_KEY
+            ) { requestKey, bundle ->
+                if (requestKey != PostContentFragment.REQUEST_KEY) return@setFragmentResultListener
+                val newPostContent = bundle.getString(
+                    PostContentFragment.REQUEST_KEY
                 )
-            } else if (args.fromFragment == REQUEST_CURRENT_POST_KEY) {
-                setFragmentResult(
-                    REQUEST_CURRENT_POST_KEY,
-                    resultBundle
-                )
+                    ?: return@setFragmentResultListener
+                viewModel.onSaveButtonClicked(newPostContent)
             }
         }
-        findNavController().popBackStack()
-    }
+//            if (args.fromFragment == REQUEST_KEY) {
+//                setFragmentResult(
+//                    REQUEST_KEY,
+//                    resultBundle
+//                )
+//            } else if (args.fromFragment == REQUEST_CURRENT_POST_KEY) {
+//                setFragmentResult(
+//                    REQUEST_CURRENT_POST_KEY,
+//                    resultBundle
+//                )
+//            }
+        }
+//        findNavController().popBackStack()
 
 
 
-     companion object {
-         const val REQUEST_KEY = "requestKey"
-         const val RESULT_KEY = "postNewContent"
-         const val RESULT_KEY_EDIT = "postEditContent"
-         const val REQUEST_CURRENT_POST_KEY = "requestForCurrentPostFragmentKey"
+    companion object {
+        const val REQUEST_KEY = "requestKey"
+        const val RESULT_KEY = "postNewContent"
+        const val RESULT_KEY_EDIT = "postEditContent"
+        const val REQUEST_CURRENT_POST_KEY = "requestForCurrentPostFragmentKey"
     }
 
 }
